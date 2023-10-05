5cf0cfc3a415eb6a7a7da79ded48b022ee3a54d2
chris wynns <christopher.wynns.521@my.csun.edu>
2023-10-05 16:13:30 -0700

    library section finished (kind of)

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -7,12 +7,13 @@ import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { doc, getDoc } from 'firebase/firestore';
 import { db } from '../firebase-config';
 
-function Dashboard() {
 
+function Dashboard() {
+  
   const [userData, setUserData] = useState(null);
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
-  const [gameData, setGameData] = useState(null);
+  const [gameData, setGameData] = useState([]);
   
 
   // making sure user is loggeed in
@@ -33,7 +34,8 @@ function Dashboard() {
         const snap = await getDoc(doc(db, 'owned_games', user.uid));
         if (snap.exists()) {
           const gameData = snap.data();
-          setGameData(gameData);
+          setGameData(gameData.games || []);
+          console.log('before gamedata:', gameData.games);
         } else {
           console.log('No such document');
         }
@@ -43,12 +45,16 @@ function Dashboard() {
         setLoading(false); // Set loading to false regardless of success or failure
       }
     };
-      // Fetch data only if user is authenticated
-      if (user) {
-        fetchGames();
-        console.log('gamedata:', gameData);
-      }
+  
+    // Fetch data only if user is authenticated
+    if (user) {
+      fetchGames();
+    }
   }, [user]);
+
+  useEffect(() => {
+    console.log('Updated gameData:', gameData);
+  }, [gameData]);
 
     // grab data from firestore with doc name matching uid
   useEffect(() => {
@@ -82,7 +88,7 @@ function Dashboard() {
     if (loading) {
       return <p>Loading user data...</p>;
     }
-
+  console.log('bottom return gameData:', gameData);
   return (
     <>
     <div className="bg-gradient-to-r from-black to-blue-800 min-h-screen">
@@ -110,10 +116,11 @@ function Dashboard() {
           </div>
           <h1 className="white-text text-2xl ml-8 pt-4">Library</h1>
           <div className="animate-slideup content-center sm:mx-10 lg:mx-20 pt-2 flex flex-wrap gap-8 sm:justify-start justify-center">
-          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((game, i) => (
-            <LibraryCard
-              key={i} // Added key prop for React to identify each element
-              {...{ appid: game, i }}
+          {gameData?.map((game, i) => (
+            <LibraryCard 
+              key={i} 
+              name={game.name}
+              appid={game.appid}
             />
           ))}
           </div>
--- src/components/LibraryCard.js
+++ src/components/LibraryCard.js
@@ -1,17 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-function LibraryCard(appid, i) {
+function LibraryCard({game, name, appid}) {
+    const [imgInfo, setImgInfo] = useState([]);
+    const [imageUrl, setImageUrl] = useState([]);
+    // grab image data from steamgriddb
+    useEffect(() => {
+        const fetchImageInfo = async () => {
+            try {
+            const response = await fetch(`http://localhost:5000/steamgrid/vert/${appid}`);
+            const img = await response.json();
+            setImgInfo(img.data);
+
+            // Move the declaration outside the if block
+            let firstImageUrl = '';
+
+            if (img.data.length > 0) {
+                firstImageUrl = img.data[0].url;
+                setImageUrl(firstImageUrl);
+                console.log('first image url: ', firstImageUrl);
+            }
+            } catch (error) {
+            console.error('Error fetching game info:', error);
+            }
+        };
+        fetchImageInfo();
+        }, [appid]);
+
   return (
     <div>
         <div class="transitioninset-0 ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 max-w-sm backdrop-blur-sm bg-white/20 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
             <a href="#">
-                <img class="rounded-t-lg" src={require('../assets/dash-small-1.jpg')} alt="" />
+                <img class="rounded-t-lg object-scale-down  w-[250px] h-[225px]" src={imageUrl} alt="" />
             </a>
             <div class="p-3">
                 <a href="#">
-                    <h5 class="mb-2 text-2xl tracking-tight text-white">Project Zomboid</h5>
+                    <h5 class="mb-2 text-xl tracking-tight text-white">{name}</h5>
                 </a>
-                <p class="font-normal text-white dark:text-gray-400">$19.99</p>
+                <p class="font-normal text-white dark:text-gray-400">{appid}</p>
             </div>
         </div>
     </div>
