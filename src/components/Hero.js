508a3399bea7acb3ec48cd64693075eaaf00329f
chris wynns <christopher.wynns.521@my.csun.edu>
2023-10-06 01:54:01 -0700

    changed hero section to look 10x better

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,26 +5,26 @@ function Hero(props) {
     <>
       <div>
         <section class="bg-gradient-to-r from-black to-blue-800 min-h-screen">
-          <div class="grid max-w-screen-xl px-4 py-5 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
-            <div class="mr-auto place-self-center lg:col-span-7">
-              <h1 class="text-white max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl ">
-                Game discovery made easy.
-              </h1>
-              <p class="max-w-2xl mb-6 font-light text-gray-300 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
-                Finding games to play with friends has never been easier.
-                Seamless integration with Steam.
-              </p>
-              <a
-                href="/login"
-                class="text-white inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
-              >
-                Get Started
-              </a>
-            </div>
-            <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
-              <img className="rounded-xl" src={require("../assets/5.jpg")} />
-            </div>
-          </div>
+        
+          <section class="bg-center bg-no-repeat bg-[url('https://images.unsplash.com/photo-1636489953081-c4ebbd50fa3a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2072&q=80')] bg-gray-700 bg-blend-multiply">
+              <div class="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
+                  <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">Game discovery made simple.</h1>
+                  <p class="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">With PlayPal, finding a game to play with your friends has never been easier. </p>
+                  <div class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
+                      <a href="#" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
+                          Get started
+                          <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                          </svg>
+                      </a>
+                      <a href="#" class="inline-flex justify-center hover:text-gray-900 items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg border border-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-400">
+                          Learn more
+                      </a>  
+                  </div>
+              </div>
+          </section>
+
+
           <div class="py-4 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
             <div class="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0">
               <div>
@@ -168,7 +168,7 @@ function Hero(props) {
               </div>
             </div>
           </div>
-        </section>
+      </section>
       </div>
     </>
   );
